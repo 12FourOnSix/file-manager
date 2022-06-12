8a63810dbc08c9072dd11c366439526d8dd3e61b
12FourOnSix <fouronsix@yandex.ru>
2022-06-12 21:33:35 +0300

    fix: changes in copy.js module

--- modules/operations/fileOps/copy.js
+++ modules/operations/fileOps/copy.js
@@ -1,17 +1,19 @@
-import { copyFile } from 'fs/promises'
+import { copyFile, stat } from 'fs/promises'
 import { constants } from 'fs'
-import { cwd } from 'process'
 import { parse, resolve } from 'path'
 import { informOfOperationFailed, informOfSuccess } from '../../accessory/talkToUser.js'
 
-export const copy = async (path_to_file, path_to_new_directory) => {
+export const copy = async (srcPath, destPath) => {
     try {
-      const 
-        srcPath = resolve(cwd(), path_to_file),
-        destBaseName = parse(path_to_new_directory).base ? '' : parse(srcPath).base,
-        destFullPath = resolve(cwd(), path_to_new_directory, destBaseName)
+      const
+        statDest = await stat(destPath),
+        isDirDest = statDest.isDirectory(),
 
-      await copyFile(srcPath, destFullPath, constants.COPYFILE_EXCL)
+        srcFullPath = resolve(srcPath),
+        srcFilename = parse(srcPath).base,
+        destFullPath = resolve(destPath, isDirDest ? srcFilename : '')
+
+      await copyFile(srcFullPath, destFullPath, constants.COPYFILE_EXCL)
       informOfSuccess()
 
     } catch (err) {
